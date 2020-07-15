74e22540c0d75b82621101f242c0ed97278e35c8
Safnas <iamsafnas@gmail.com>
2020-07-15 21:09:40 +0530

    submit form handling and initbinder for string trimming

--- src/com/safnas/springdemo/mvc/CustomerController.java
+++ src/com/safnas/springdemo/mvc/CustomerController.java
@@ -2,29 +2,37 @@ package com.safnas.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-	
+
+	// init method to remove leading and trailing space
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+
 	@RequestMapping("/showForm")
-	public String showForm(Model theModel){
-		
-		theModel.addAttribute("customer",new Customer());
-		
+	public String showForm(Model theModel) {
+
+		theModel.addAttribute("customer", new Customer());
+
 		return "customer-form";
 	}
-	
+
 	@RequestMapping("/processForm")
-	public String processForm(
-			@Valid @ModelAttribute("customer") Customer theCustomer,
-			BindingResult bindingResult){
-		if(bindingResult.hasErrors()){
+	public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
 			return "customer-form";
 		}
 		return "customer-confirmation";
